146423b4b699d9d348162e2a20d1d3f2207c7ccc
RaulBirlem <raul.birlem@gmail.com>
2024-03-14 13:14:42 -0300

    Creating NotificationService

--- picpaysimplificado/src/main/java/com/picpaysimplificado/services/TransactionService.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -26,7 +26,10 @@ public class TransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void createTransaction(TransactionDTO transaction) throws Exception {
+    @Autowired
+    private NotificationService notificationService;
+
+    public Transaction createTransaction(TransactionDTO transaction) throws Exception {
         //recebe os payload para a criação da transação e validações
         User sender = this.userService.findUserById(transaction.senderId());
         User receiver = this.userService.findUserById(transaction.receiverId());
@@ -54,6 +57,11 @@ public class TransactionService {
         this.repository.save(newTransaction);
         this.userService.saveUser(sender);
         this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "Transação realizada com sucesso.");
+        this.notificationService.sendNotification(receiver, "Transação recebida com sucesso.");
+
+        return newTransaction;
     }
 
     public boolean authorizeTransaction(User sender, BigDecimal value){
@@ -63,5 +71,6 @@ public class TransactionService {
             String message = (String) authorizationResponse.getBody().get("message");
             return "Autorizado".equalsIgnoreCase(message);
         } else return false;
+
     }
 }
