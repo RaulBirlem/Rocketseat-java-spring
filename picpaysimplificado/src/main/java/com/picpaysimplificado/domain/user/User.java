a565bbf408e23fb6fa25da51bac6fd1ce3ce75ff
RaulBirlem <raul.birlem@gmail.com>
2024-03-14 13:16:00 -0300

    add userDTO

--- picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,11 +1,9 @@
 package com.picpaysimplificado.domain.user;
 
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,12 +12,13 @@ import java.math.BigDecimal;
 @Getter    //  lombot
 @Setter
 @AllArgsConstructor   // gera constructor q recebe os parâmetros da classe
+@NoArgsConstructor
 @EqualsAndHashCode(of="id") //chave primária da tabela
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //gera id de forma incremental
     private Long id;
-    private String fisrtName;
+    private String firstName;
     private String lastName;
     @Column(unique=true)
     private String document;
@@ -29,4 +28,13 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING) // userType representa um dos valores criados
     private UserType userType;
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.document = data.document();
+        this.email = data.email();
+    }
 }
