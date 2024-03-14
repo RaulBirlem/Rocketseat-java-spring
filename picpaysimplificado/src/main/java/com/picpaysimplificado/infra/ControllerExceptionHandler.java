66db07219d6c7e304debfbf24e2498def843d053
RaulBirlem <raul.birlem@gmail.com>
2024-03-14 13:16:34 -0300

    Creating ControllerExceptionHandler

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.picpaysimplificado.infra;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        return ResponseEntity.badRequest().build();
+    }
+}
